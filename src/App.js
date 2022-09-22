1e6ccaa3a49f85e235652f1b95abb4d8814f4b07
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-09-22 18:46:39 +0530

    feat: question updation, show score functionality.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import Questions from './Components/Questions';
+import {v4 as uuidv4} from "uuid";
+
 
 const  App = () => {
-  return <div className="App">App</div>
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const [showScore, setShowScore] = useState(false);
+
+
+  const handleCorrectAnswer = (isCorrect) => {
+    if(isCorrect) {
+      setScore(score + 1);
+    }
+    setClicked(true);
+  };
+
+  const  handleNextQuestion = () => {
+    setClicked(false);
+    if(currentQuestion < Questions.length-1){
+      setCurrentQuestion(currentQuestion+1);
+    }else{
+      setShowScore(true);
+    }
+  };
+
+  return <div className="app-wrapper">
+
+    {showScore ? (
+      <div>
+        <div className="completed">Completed</div>
+        <div className="score-section">
+          Your Score : {score}/{Questions.length}
+        </div>
+     </div>
+    ):(
+    <div>
+      <div className="question-section-wrapper">
+        <div className="question-count">
+          Question {currentQuestion + 1} of {Questions.length}
+        </div>
+        <div className="question">
+          {Questions[currentQuestion].question}
+        </div>
+      </div>
+
+      <div className="answer-section-wrapper">
+        {Questions[currentQuestion].answers.map((answerOption) =>
+        (
+           <li className="answer-list" key={uuidv4()}>
+            <button className={`answer-button ${clicked &&  answerOption.isCorrect ? "correct": "" }`}
+             onClick={()=>handleCorrectAnswer(answerOption.isCorrect)}>{answerOption.answer}</button>
+           </li>
+        ))}
+      </div>
+
+    <div>
+      <button className="next-button" onClick={handleNextQuestion}>Next</button>
+    </div>
+    </div>
+    )}
+  </div>
 }
 
 export default App;
---
+++ src/Components/Questions.js
@@ -0,0 +1,57 @@
+const Questions = [
+{
+    question: "1. Which of the following is the correct name of React.js?",
+    answers: [
+        {answer: "React" , isCorrect: false},
+        {answer: "ReactJS" , isCorrect: false},
+        {answer: "React.js" , isCorrect: false},
+        {answer: "All of the above" , isCorrect: true},
+    ],
+},
+
+{
+    question: "2. Which of the following command is used to install create-react-app?",
+    answers: [
+        {answer: "npm install -g create-react-app" , isCorrect: true},
+        {answer: "npm install create-react-app" , isCorrect: false},
+        {answer: "npm install -f create-react-app" , isCorrect: false},
+        {answer: "install -g create-react-app" , isCorrect: false},
+    ],
+},
+
+
+{
+    question: "3. What of the following is used in React.js to increase performance?",
+    answers: [
+        {answer: "Original DOM" , isCorrect: false},
+        {answer: "Virtual DOM" , isCorrect: true},
+        {answer: "Both A and B" , isCorrect: false},
+        {answer: "None of the above" , isCorrect: false},
+    ],
+},
+
+
+{
+    question: "4. Which of the following acts as the input of a class-based component?",
+    answers: [
+        {answer: "Class" , isCorrect: false},
+        {answer: "Factory" , isCorrect: false},
+        {answer: "Render" , isCorrect: false},
+        {answer: "Props" , isCorrect: true},
+    ],
+},
+
+{
+    question: "5. How many numbers of elements a valid react component can return",
+    answers: [
+        {answer: "1" , isCorrect: true},
+        {answer: "2" , isCorrect: false},
+        {answer: "4" , isCorrect: false},
+        {answer: "5" , isCorrect: true},
+    ],
+},
+
+]
+
+
+export default Questions;
